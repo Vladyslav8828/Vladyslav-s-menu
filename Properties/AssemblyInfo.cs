dd75312a399c4bcd0fc91a24b35e995fe153996e
Vladyslav <persorck@gmail.com>
2026-08-24 00:36:40 +0100

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MalachiTemp")]
-[assembly: AssemblyDescription("A Perfect Template Made By Malachi")]
+[assembly: AssemblyTitle("Vladyslavs menu")]
+[assembly: AssemblyDescription("A mod menu by vladyslav made with malachi template")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Project Malachi")]
-[assembly: AssemblyProduct("MalachiTemp")]
+[assembly: AssemblyCompany("Vladyslav")]
+[assembly: AssemblyProduct("Vladyslavs menu")]
 [assembly: AssemblyCopyright("Copyright Project Malachi©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
